abe963f1b58eea13e12d6dcc9089b021b928eb2a
VasiliiSokolov <vasilysokolov@mail.ru>
2022-08-22 18:33:25 +0300

    Done!

--- Condominium/Condominium.cpp
+++ Condominium/Condominium.cpp
@@ -1,21 +1,27 @@
-﻿// Condominium.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+bool check_number(int);
 
 int main()
 {
-    std::string condominium{ 10 };
-    std::cout << "Hello World!\n";
+    std::string condominium[10];
+    int apartmentNumber;
+    std::cout << "Input tenants:" << std::endl;
+    
+    for (int count = 0; count < 10; count++)
+    {
+        std::cin >> condominium[count];
+    }
+    for (int count = 0; count < 3; count++)
+    {
+        std::cout << "Input apartment number(0 - 9):" << std::endl;
+        std::cin >> apartmentNumber;
+        if (!check_number(apartmentNumber)) break;
+        std::cout << condominium[apartmentNumber] << std::endl;
+    }
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+bool check_number(int apartmentNumber)
+{
+    if (apartmentNumber < 0 || apartmentNumber > 9) return false;
+    else return true;
+}
